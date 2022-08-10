173d5848b439dcecbb0465113c6eeaebeeb28a0b
Sentayhu berhanu <sentayhuberhanu19+github@gmail.com>
2022-08-10 10:21:39 +0300

    Linters, redux, and components created

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { Provider } from 'react-redux';
+import {  BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Greetings from './Greetings';
+import store from '../redux/configureStore';
+import { fetchdata } from '../redux/reactrails/actions';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+
+const App = () => (
+  <div className="App">
+    <Provider store={store}>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Greetings />} />
+        </Routes>
+      </Router>
+    </Provider>
+  </div>
+);
 
 export default App;
